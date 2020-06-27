647183064f44fbf7caa33be8014d83c0754bfd0e
mihalo3c@gmail.com <mihalo3c@gmail.com>
2020-06-27 21:25:05 +0200

    Issue #111

--- main.py
+++ main.py
@@ -2,14 +2,26 @@
 
 
 import json
+import time
+import requests
+import os
 
 from binance_bot import BinanceBot, Plan
 import helpers as hp
 from config import SLACK_KEY, SLACK_GROUP, DEPLOYMENT_SETTINGS_SOURCE
-import time
 
 
 def main(settings, execute=1, test_it=0, loop=1):
+
+    def run_bb_bot(plans):
+        bb = BinanceBot(plans, execute=execute, test_it=test_it, loop=loop, settings=settings["global_settings"])
+        bb.start_listening()
+        timeout = 3600 * 4
+        while True:
+            time.sleep(timeout)
+            msg = f"Bot is alive, last update was {time.time() - bb.last_book_update} sec ago."
+            hp.send_to_slack(msg, SLACK_KEY, SLACK_GROUP, emoji=":blocky-robot:")
+
     # deploymentSettings --> plans
     plans = []
     for plan in settings["plans"]:
@@ -20,17 +32,23 @@ def main(settings, execute=1, test_it=0, loop=1):
                           strategy="ARBITRAGE",
                           profit_asset=plan["profit_asset"],
                           fee_asset=plan["fee_asset"]))
-    try:
-        bb = BinanceBot(plans, execute=execute, test_it=test_it, loop=loop, settings=settings["global_settings"])
-        bb.start_listening()
-        timeout = 3600 * 4
-        while True:
-            time.sleep(timeout)
-            msg = f"Bot is alive, last update was {time.time() - bb.last_book_update} sec ago."
-            hp.send_to_slack(msg, SLACK_KEY, SLACK_GROUP, emoji=":blocky-grin:")
-    except Exception as e:
-        hp.send_to_slack(str(e), SLACK_KEY, SLACK_GROUP, emoji=":blocky-grin:")
-        exit()
+
+    while 1:
+        try:
+            run_bb_bot(plans)  # Runs in an infinite loop
+        except Exception as e:
+            try:
+                hp.send_to_slack(str(e), SLACK_KEY, SLACK_GROUP, emoji=":blocky-grin:")
+            except requests.exceptions.ConnectionError:
+                timestamp = time.time()
+                url = "https://slack.com"
+                while 1:
+                    response = os.system(f"ping {url}")
+                    if response == 0:  # If response is a success
+                        msg = f"Connection was disturbed for {time.time() - timestamp}"
+                        hp.send_to_slack(msg, SLACK_KEY, SLACK_GROUP, emoji=":blocky-grin:")
+                        break
+                    time.sleep(20)  # Try again in 20 sec
 
 
 if __name__ == "__main__":
